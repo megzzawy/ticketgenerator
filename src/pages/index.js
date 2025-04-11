295cf9392e0a35ed1b85bbbfab8b3107d780aa9c
megzzawy <amgadsameh99@gmail.com>
2025-04-11 15:20:57 +0200

    fixing

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [userData, setUserData] = useState(null);
   return (
     <div className="relative flex justify-center bg-[url('/background-mobile.png')] md:bg-[url('/background-tablet.png')] xl:bg-[url('/background-desktop.png')] bg-cover bg-center'">  
-      {userData ? user && <Ticket user={userData} /> : <Form onSubmit={setUserData} />}
+      {userData ? <Ticket user={userData} /> : <Form onSubmit={setUserData} />}
       <Image src="/pattern-lines.svg" alt="Picture of the author" width={500} height={100} className='h-screen w-auto absolute -top-40' />
       <Image src="/pattern-squiggly-line-bottom-desktop.svg" alt="Picture of the author" width={100} height={100} className='h-100 w-auto absolute bottom-0 left-0' />
       <Image src="/pattern-lines.svg" alt="Picture of the author" width={500} height={100} className='h-screen w-auto absolute -top-40' />
